c315aa0958853c4e09f55e6cf6154dceac0a731f
ElMarchk0 <67571255+ElMarchk0@users.noreply.github.com>
2021-01-04 21:51:04 -0800

    Express

--- index.js
+++ index.js
@@ -1,63 +1,22 @@
-const http = require('http')
-const url = require('url')
-const fs = require('fs')
+const express = require('express')
 const path = require('path')
 
-const server = http.createServer((req, res) => {
-    let filePath = path.join(
-        __dirname,
-        "src",
-        req.url === "/" ? "index.html" : req.url
-    )
+const app = express()
 
-    let extname = path.extname(filePath)
+app.use(express.static(path.join(__dirname, 'public')))
 
-    let contentType = "text/html"
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-    switch (extname) {
-        case ".js":
-            contentType = "text/javascript";
-            break;
-        case ".css":
-            contentType = "text/css";
-            break;
-        case ".json":
-            contentType = "application/json";
-            break;
-        case ".png":
-            contentType = "image/png";
-            break;
-        case ".jpg":
-            contentType = "image/jpg";
-            break;
-    }
+app.get('./public/api/beersV1.json', (req, res) =>
+    res.render('index', {
+        title: 'Beer Api',
+        beers
+    })
+);
 
-    if (contentType == "text/html" && extname == "") filePath += ".html";
-
-    fs.readFile(filePath, (err, content) => {
-        if (err) {
-            if (err.code == "ENOENT") {
-                // Page not found
-                fs.readFile(
-                    path.join(__dirname, "public", "404.html"),
-                    (err, content) => {
-                        res.writeHead(404, { "Content-Type": "text/html" });
-                        res.end(content, "utf8");
-                    }
-                );
-            } else {
-                //  Some server error
-                res.writeHead(500);
-                res.end(`Server Error: ${err.code}`);
-            }
-        } else {
-            // Success
-            res.writeHead(200, { "Content-Type": contentType });
-            res.end(content, "utf8");
-        }
-    });
-})
+// app.use('/api/beersV1.json', require('./public/api/beersV1.json'))
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
